614b998be43532decc889995651a5954b0229422
TonySu479 <tonysu479@gmail.com>
2021-01-28 20:40:56 +0100

    First Commit

---
+++ app/src/main/java/com/example/happyplaces/AddHappyPlaceActivity.kt
@@ -0,0 +1,13 @@
+package com.example.happyplaces
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_add_happy_place.*
+
+class AddHappyPlaceActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_happy_place)
+        setSupportActionBar(toolbar_add_place)
+    }
+}
