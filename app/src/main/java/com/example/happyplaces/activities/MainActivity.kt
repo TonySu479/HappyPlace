93f354a1b4bca62229e19547f91b1623eb8c8e29
TonySu479 <tonysu479@gmail.com>
2021-02-01 13:34:56 +0100

    Swipe Edit

--- app/src/main/java/com/example/happyplaces/activities/MainActivity.kt
+++ app/src/main/java/com/example/happyplaces/activities/MainActivity.kt
@@ -6,12 +6,15 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.happyplaces.R
 import com.example.happyplaces.database.DatabaseHandler
 import com.example.happyplaces.models.HappyPlaceModel
 import com.happyplaces.adapters.HappyPlacesAdapter
 import kotlinx.android.synthetic.main.activity_main.*
+import pl.kitek.rvswipetodelete.SwipeToEditCallback
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,22 +27,35 @@ class MainActivity : AppCompatActivity() {
         getHappyPlacesListFromLocalDB()
     }
 
-    private fun setupHappyPlacesRecyclerView(
-        happyPlaceList: ArrayList<HappyPlaceModel>
-    ) {
+    private fun setupHappyPlacesRecyclerView(happyPlacesList: ArrayList<HappyPlaceModel>) {
         rv_happy_places_list.layoutManager = LinearLayoutManager(this)
         rv_happy_places_list.setHasFixedSize(true)
 
-        val placesAdapter = HappyPlacesAdapter(this, happyPlaceList)
+        val placesAdapter = HappyPlacesAdapter(this, happyPlacesList)
         rv_happy_places_list.adapter = placesAdapter
 
-        placesAdapter.setOnClickListener(object : HappyPlacesAdapter.OnClickListener{
+        placesAdapter.setOnClickListener(object :
+            HappyPlacesAdapter.OnClickListener {
             override fun onClick(position: Int, model: HappyPlaceModel) {
                 val intent = Intent(this@MainActivity, HappyPlaceDetailActivity::class.java)
-                intent.putExtra(EXTRA_PLACE_DETAILS, model)
+                intent.putExtra(EXTRA_PLACE_DETAILS, model) // Passing the complete serializable data class to the detail activity using intent.
                 startActivity(intent)
             }
         })
+
+        val editSwipeHandler = object : SwipeToEditCallback(this) {
+            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+
+                val adapter = rv_happy_places_list.adapter as HappyPlacesAdapter
+                adapter.notifyEditItem(
+                    this@MainActivity,
+                    viewHolder.adapterPosition,
+                    ADD_PLACE_ACTIVITY_REQUEST_CODE
+                )
+            }
+        }
+        val editItemTouchHelper = ItemTouchHelper(editSwipeHandler)
+        editItemTouchHelper.attachToRecyclerView(rv_happy_places_list)
     }
 
     private fun getHappyPlacesListFromLocalDB() {
@@ -66,6 +82,9 @@ class MainActivity : AppCompatActivity() {
             }
         }
     }
+
+
+
 
     companion object {
         var ADD_PLACE_ACTIVITY_REQUEST_CODE = 1
